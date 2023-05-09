61dc3999348d5c6ad2a3e0b0c49a9ee3411d83e4
NQuocNk20 <102974265+NQuocNk20@users.noreply.github.com>
2023-05-10 02:50:56 +0700

    animate scroll

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-const adressBtn = document.querySelector('#adress-form'); 
-const adressClose = document.querySelector('#adress-close'); 
+// -------------- Scroll To Top ---------------
+function scrollToTop () {
+    window.scrollTo(0, 0)
+}
+
+// ------------------------
+
+const adressBtn = document.querySelector('#adress-form');
+const adressClose = document.querySelector('#adress-close');
 
 // console.log(adressBtn)
 
-adressBtn.addEventListener("click", function() {
+adressBtn.addEventListener("click", function () {
     document.querySelector('.adress-form').style.display = "flex";
 })
 
-adressClose.addEventListener("click", function() {
+adressClose.addEventListener("click", function () {
     document.querySelector('.adress-form').style.display = "none";
 })
 
@@ -21,19 +28,19 @@ const imgNumber = document.querySelectorAll(".slider-content-left-top img");
 let index = 0;
 // console.log(imgNumber.length)
 
-rightBtn.addEventListener("click", function() {
+rightBtn.addEventListener("click", function () {
     // console.log("oke")
-    index = index+1;
-    if(index > imgNumber.length-1) {
+    index = index + 1;
+    if (index > imgNumber.length - 1) {
         index = 0;
     }
     document.querySelector(".slider-content-left-top").style.right = index * 100 + "%";
 })
 
-leftBtn.addEventListener("click", function() {
-    index = index-1
-    if(index < 0) {
-        index = imgNumber.length-1;
+leftBtn.addEventListener("click", function () {
+    index = index - 1
+    if (index < 0) {
+        index = imgNumber.length - 1;
     }
     document.querySelector(".slider-content-left-top").style.right = index * 100 + "%";
 })
@@ -45,8 +52,8 @@ leftBtn.addEventListener("click", function() {
 const imgNumberLi = document.querySelectorAll(".slider-content-left-bottom li")
 // console.log(imgNumberLi)
 
-imgNumberLi.forEach(function(img, index) {
-    img.addEventListener('click', function() {
+imgNumberLi.forEach(function (img, index) {
+    img.addEventListener('click', function () {
         removeActive();
         document.querySelector(".slider-content-left-top").style.right = index * 100 + "%";
         img.classList.add("active")
@@ -63,7 +70,7 @@ function removeActive() {
 //  ------------------------------ Slider auto slide --------------------------
 function imgAuto() {
     index = index + 1;
-    if(index > imgNumber.length-1) {
+    if (index > imgNumber.length - 1) {
         index = 0;
     }
     removeActive();
@@ -81,19 +88,92 @@ const rightBtnPro = document.querySelector(".fa-chevron-right-Pro");
 const leftBtnPro = document.querySelector(".fa-chevron-left-Pro");
 const imgNumberPro = document.querySelectorAll(".slider-product-one-content-items");
 
-rightBtnPro.addEventListener("click", function() {
+rightBtnPro.addEventListener("click", function () {
     // console.log("oke")
-    index = index+1;
-    if(index > imgNumberPro.length-1) {
+    index = index + 1;
+    if (index > imgNumberPro.length - 1) {
         index = 0;
     }
     document.querySelector(".slider-product-one-content-items-content").style.right = index * 100 + "%";
 })
 
-leftBtnPro.addEventListener("click", function() {
-    index = index-1
-    if(index <= 0) {
-        index = imgNumberPro.length-1;
+leftBtnPro.addEventListener("click", function () {
+    index = index - 1
+    if (index <= 0) {
+        index = imgNumberPro.length - 1;
     }
     document.querySelector(".slider-product-one-content-items-content").style.right = index * 100 + "%";
 })
+
+
+// ----------------- Scroll animate -------------------
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         if(entry.isIntersecting) {
+//             entry.target.classList.add('show');
+//         } else {
+//             entry.target.classList.remove('show');
+//         }
+//     });
+// });
+
+// const sectionElement = document.querySelector('section.img');
+// const childElements = sectionElement.querySelectorAll('.text');
+// childElements.forEach((el) => {
+//     el.classList.add('hidden');
+//     observer.observe(el);
+// });
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.remove('hidden');
+//         } else {
+//             entry.target.classList.add('hidden');
+//         }
+//     });
+// });
+
+// const sections = document.querySelectorAll('.container .img, .container . .text');
+// sections.forEach((section) => {
+//     observer.observe(section);
+//     section.classList.add('hidden');
+// });
+
+
+
+const observerProduct = new IntersectionObserver((entriesPRO) => {
+    entriesPRO.forEach((entryPro) => {
+        if (entryPro.isIntersecting) {
+            entryPro.target.classList.add('showRow');
+        } else {
+            entryPro.target.classList.remove('showRow');
+        }
+    });
+});
+
+const hiddenProGalle = document.querySelector('.product-gallery-content-product');
+const childElementsProduct = hiddenProGalle.querySelectorAll('.product-gallery-content-product-item');
+childElementsProduct.forEach((el) => {
+    el.classList.add('hiddenRow');
+    observerProduct.observe(el);
+});
+
+// const hiddenElements = document.querySelectorAll('.hidden');
+// hiddenElements.forEach((el) => observer.observe(el));
+
+
+const observerRow = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        // console.log(entry1)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('showRow');
+        } else {
+            entry.target.classList.remove('showRow');
+        }
+    });
+});
+
+const hiddenElementsRow = document.querySelectorAll('.hiddenRow');
+hiddenElementsRow.forEach((el) => observerRow.observe(el));
